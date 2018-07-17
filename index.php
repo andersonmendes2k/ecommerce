34e8358e10d1a08c0516d48c98c9ea7951e81a8a
Anderson Mendes <andersonmendes2k@gmail.com>
2018-07-17 18:24:56 -0300

    admin login

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php 
-
+session_start();
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Ands\Page;
 use \Ands\PageAdmin;
+use \Ands\Model\User;
 
 $app = new Slim();
 
@@ -20,9 +21,38 @@ $app->get('/', function() {
 
 $app->get('/adm', function() {
 
+	User::verifyLogin();
+
 	$page = new PageAdmin();
 
 	$page->setTpl("index");
+
+});
+
+$app->get('/adm/login', function() {
+
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("login");
+});
+
+$app->post('/adm/login', function() {
+	
+	User::login($_POST["login"], $_POST["password"]);
+
+	header("Location: /adm");
+	exit;
+
+});
+
+$app->get('/adm/logout', function(){
+
+	User::logout();
+	header("Location: /adm/login");
+	exit;
 
 });
 
