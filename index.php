5c452505f42ae261011c72c9419248d208fb68b3
Anderson Mendes <andersonmendes2k@gmail.com>
2018-07-17 15:07:04 -0300

    admin page

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Ands\Page;
+use \Ands\PageAdmin;
 
 $app = new Slim();
 
@@ -12,6 +13,14 @@ $app->config('debug', true);
 $app->get('/', function() {
 
 	$page = new Page();
+
+	$page->setTpl("index");
+
+});
+
+$app->get('/adm', function() {
+
+	$page = new PageAdmin();
 
 	$page->setTpl("index");
 
